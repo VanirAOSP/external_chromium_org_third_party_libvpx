f2af7c132172a48caa3062a6556c6990d2184686
Android Chromium Automerger <chromium-automerger@android>
2014-05-08 06:34:35 +0000

    Update makefiles after merge of Chromium at 269041
    This commit was generated by merge_from_chromium.py.
    
    Change-Id: I921c9ba23e14751ae8f9dc95986980051160b6a9

--- libvpx_obj_int_extract.host.darwin-arm.mk
+++ libvpx_obj_int_extract.host.darwin-arm.mk
@@ -64,8 +64,6 @@ MY_DEFS_Debug := \
 	'-DCLD_VERSION=1' \
 	'-DENABLE_PRINTING=1' \
 	'-DENABLE_MANAGED_USERS=1' \
-	'-DDATA_REDUCTION_FALLBACK_HOST="http://compress.googlezip.net:80/"' \
-	'-DSPDY_PROXY_AUTH_ORIGIN="https://proxy.googlezip.net:443/"' \
 	'-DFORCE_PARSE_ELF' \
 	'-DUSE_OPENSSL=1' \
 	'-DUSE_OPENSSL_CERTS=1' \
@@ -127,8 +125,6 @@ MY_DEFS_Release := \
 	'-DCLD_VERSION=1' \
 	'-DENABLE_PRINTING=1' \
 	'-DENABLE_MANAGED_USERS=1' \
-	'-DDATA_REDUCTION_FALLBACK_HOST="http://compress.googlezip.net:80/"' \
-	'-DSPDY_PROXY_AUTH_ORIGIN="https://proxy.googlezip.net:443/"' \
 	'-DFORCE_PARSE_ELF' \
 	'-DUSE_OPENSSL=1' \
 	'-DUSE_OPENSSL_CERTS=1' \
